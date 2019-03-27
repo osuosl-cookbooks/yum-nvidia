94bba48b93a34d077e12e4cce7e94499c7a300af
Lance Albertson <lance@osuosl.org>
2019-03-27 12:23:07 -0700

    Add support for ppc64le using OSL repositories

--- attributes/libnvidia-container.rb
+++ attributes/libnvidia-container.rb
@@ -1,13 +1,21 @@
-default['yum']['libnvidia-container']['baseurl'] =
-  'https://nvidia.github.io/libnvidia-container/centos$releasever/$basearch'
+case node['kernel']['machine']
+when 'ppc64le'
+  default['yum']['libnvidia-container']['baseurl'] =
+    'https://ftp.osuosl.org/pub/osl/repos/yum/$releasever/libnvidia-container/$basearch'
+  default['yum']['libnvidia-container']['gpgcheck'] = true
+  default['yum']['libnvidia-container']['gpgkey'] = 'https://ftp.osuosl.org/pub/osl/repos/yum/RPM-GPG-KEY-osuosl'
+else
+  default['yum']['libnvidia-container']['baseurl'] =
+    'https://nvidia.github.io/libnvidia-container/centos$releasever/$basearch'
+  default['yum']['libnvidia-container']['gpgcheck'] = false
+  default['yum']['libnvidia-container']['gpgkey'] = 'https://nvidia.github.io/libnvidia-container/gpgkey'
+  default['yum']['libnvidia-container']['repo_gpgcheck'] = true
+  default['yum']['libnvidia-container']['sslcacert'] = '/etc/pki/tls/certs/ca-bundle.crt'
+  default['yum']['libnvidia-container']['sslverify'] = true
+end
 default['yum']['libnvidia-container']['description'] = 'libnvidia-container'
 default['yum']['libnvidia-container']['enabled'] = true
 default['yum']['libnvidia-container']['failovermethod'] = 'priority'
-default['yum']['libnvidia-container']['gpgcheck'] = false
-default['yum']['libnvidia-container']['gpgkey'] = 'https://nvidia.github.io/libnvidia-container/gpgkey'
 default['yum']['libnvidia-container']['make_cache'] = true
 default['yum']['libnvidia-container']['managed'] = true
-default['yum']['libnvidia-container']['repo_gpgcheck'] = true
 default['yum']['libnvidia-container']['repositoryid'] = 'libnvidia-container'
-default['yum']['libnvidia-container']['sslcacert'] = '/etc/pki/tls/certs/ca-bundle.crt'
-default['yum']['libnvidia-container']['sslverify'] = true

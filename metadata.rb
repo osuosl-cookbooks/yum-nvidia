33c31a70bf64f4ca99f76e4fe45aa8fb12873ffb
Robert Detjens <detjensrobert@osuosl.org>
2019-12-22 18:14:30 -0800

    bump chef_version to >= 14.0

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'yum-nvidia'
 maintainer       'Oregon State University'
 maintainer_email 'chef@osuosl.org'
 license          'Apache-2.0'
-chef_version     '>= 12.18' if respond_to?(:chef_version)
+chef_version     '>= 14.0'
 issues_url       'https://github.com/osuosl-cookbooks/yum-nvidia/issues'
 source_url       'https://github.com/osuosl-cookbooks/yum-nvidia'
 description      'Installs/Configures yum-nvidia'
